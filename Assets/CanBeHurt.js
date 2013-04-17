ad6cc0e584840ee3c7e628f6d11ecfe449d340bf
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-17 16:30:50 -0400

    Now remote health?

--- Assets/CanBeHurt.js
+++ Assets/CanBeHurt.js
@@ -3,5 +3,12 @@
 var health : float = 100.0;
 
 function BeHurt(damage : float) {
-	health -= damage;
+	networkView.RPC("RemoteBeHurt", RPCMode.AllBuffered, networkView.viewID);
+}
+
+@RPC
+function RemoteBeHurt(view : NetworkView) {
+	var view : NetworkView = NetworkView.Find(view);
+	var beHurtCpt : CanBeHurt = view.GetComponent(CanBeHurt) as CanBeHurt;
+	beHurtCpt.health -= damage;
 }
