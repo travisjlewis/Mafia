36e01e6b73a55c2c18b09a7a26e7c8b579b2c970
Julian Ceipek <julian.ceipek@gmail.com>
2013-04-17 16:42:03 -0400

    Some script tweaks

--- Assets/CanBeHurt.js
+++ Assets/CanBeHurt.js
@@ -3,12 +3,12 @@
 var health : float = 100.0;
 
 function BeHurt(damage : float) {
-	networkView.RPC("RemoteBeHurt", RPCMode.AllBuffered, networkView.viewID);
+	networkView.RPC("RemoteBeHurt", RPCMode.AllBuffered, networkView.viewID, damage);
 }
 
 @RPC
-function RemoteBeHurt(view : NetworkView) {
-	var view : NetworkView = NetworkView.Find(view);
+function RemoteBeHurt(viewID : NetworkViewID, damage : float) {
+	var view : NetworkView = NetworkView.Find(viewID);
 	var beHurtCpt : CanBeHurt = view.GetComponent(CanBeHurt) as CanBeHurt;
 	beHurtCpt.health -= damage;
 }
